f14a7deb78940d87a84302c2a2836a466be320e0
FPSensor <gkartytgmail.com>
2023-01-30 21:53:56 +0000

    r3q: set treble vndk version to current

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -86,6 +86,9 @@ SYSTEM_EXT_PRIVATE_SEPOLICY_DIRS += \
 
 BOARD_VENDOR_SEPOLICY_DIRS += $(COMMON_PATH)/sepolicy/vendor
 
+# Treble
+BOARD_VNDK_VERSION := current
+
 # Verified Boot
 BOARD_AVB_ENABLE := true
 BOARD_AVB_MAKE_VBMETA_IMAGE_ARGS += --set_hashtree_disabled_flag
